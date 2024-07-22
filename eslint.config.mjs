d5e2b89e52c5e4cf288a668d88e9d7437ab8e407
Nicholas Sorokin <nicholas.sorokin@hotmail.com>
2024-07-22 11:39:31 +0930

    Add updated template files

---
+++ eslint.config.mjs
@@ -0,0 +1,58 @@
+import reactRefresh from 'eslint-plugin-react-refresh';
+import globals from 'globals';
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import reactHooks from 'eslint-plugin-react-hooks';
+import pluginPrettier from 'eslint-plugin-prettier';
+import configPrettier from 'eslint-config-prettier';
+
+export default tseslint.config(
+	{
+		files: ['**/*.ts', '**/*.tsx'],
+		ignores: ['**/dist', 'eslint.config.mjs', 'vite.config.ts'],
+		languageOptions: {
+			parser: tseslint.parser,
+			parserOptions: {
+				project: ['./tsconfig.app.json'],
+			},
+			globals: {
+				...globals.browser,
+				...globals.es2024,
+			},
+		},
+		extends: tseslint.configs.recommendedTypeChecked,
+	},
+	eslint.configs.recommended,
+	{
+		plugins: { prettier: pluginPrettier },
+		.../** @type {import('typescript-eslint').ConfigWithExtends} */ (
+			configPrettier
+		),
+	},
+	{
+		plugins: { 'react-hooks': reactHooks },
+		rules: {
+			'react-hooks/rules-of-hooks': 'error',
+			'react-hooks/exhaustive-deps': 'warn',
+		},
+	},
+	{
+		plugins: {
+			'react-refresh': reactRefresh,
+		},
+
+		rules: {
+			'react-refresh/only-export-components': [
+				'warn',
+				{
+					allowConstantExport: true,
+				},
+			],
+		},
+	},
+	{
+		rules: {
+			'no-unused-vars': 'off',
+		},
+	}
+);
