94f032644532cbe1695a0e0d1f6ffb2832a9c3b2
Nicholas Sorokin <nicholas.sorokin@hotmail.com>
2026-04-09 23:55:04 +0930

    ci: update ci

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: '/pandemic_tracker',
+	base: process.env.VITE_BASE_URL || '/',
 	build: {
 		sourcemap: 'hidden',
 	},
